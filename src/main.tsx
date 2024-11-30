910df3119021a253fe380c5256d3e988dfb0b972
chimaux <ugoppromise@gmail.com>
2024-11-30 18:06:58 +0100

    Air reading page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Suspense fallback={<div>Loading...</div>}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Suspense>
+  </StrictMode>
+);
--- src/store/airReading.ts
+++ src/store/airReading.ts
@@ -1,50 +1,98 @@
-import { AQIData } from "../types/airMonitoring";
+import { AMD_type_v2 } from "../types/airMonitoring";
 
 import { create } from "zustand";
 
 interface amt_data_type{
-  AQI_datas:AQIData[];
-  set_AQI_datas:(state:AQIData[])=>void;
-  filtered_AQI_datas:AQIData[];
-  set_filtered_AQI_datas:(state:AQIData[])=>void;
+  AQI_datas:AMD_type_v2[] | undefined;
+  set_AQI_datas:(state:AMD_type_v2[] | undefined)=>void;
+  filtered_AQI_datas:AMD_type_v2[] | undefined;
+  set_filtered_AQI_datas:(state:AMD_type_v2[] | undefined)=>void;
 }
 const useAqtStore = create<amt_data_type>((set) => ({
-  AQI_datas:[
+  AQI_datas: [
     {
       id: "",
-      country: "",
-      state: "",
-      lga: "",
-      city: "",
-      AQI: "",
-      pm_1: "",
-      pm_2: "",
-      pm10: "",
-      temp: "",
-      humidity: "",
-      heat: "",
-      voltage: "",
-      date:"",
-    },
+      device_uid: "",
+      serial_number: "",
+      location: "",
+      lat: 0,
+      lon:0,
+      createdAt:"",
+     updatedAt: "",
+      airReading: [
+        {
+          id: "",
+          aqi: 0,
+          humidity:0,
+          pm01_0: 0,
+          pm02_5: 0,
+          pm10_0: 0,
+          pressure: 0,
+          temperature: 0,
+          voltage: 0,
+          device_uid: "",
+          captured: 0,
+          createdAt: "",
+          updatedAt: "",
+          airQualityReadingId: ""
+        },
+       
+      ],
+      histories: [
+        {
+          id: "",
+          date: "",
+          aqi: 0,
+          pm1_0: 0,
+          pm2_5: 0,
+          pm10_0: 0,
+          aiReadingId: ""
+        },
+      ]
+    }
   ],
 set_AQI_datas: (state) => set({ AQI_datas: state }),
   filtered_AQI_datas:[
     {
       id: "",
-      country: "",
-      state: "",
-      lga: "",
-      city: "",
-      AQI: "",
-      pm_1: "",
-      pm_2: "",
-      pm10: "",
-      temp: "",
-      humidity: "",
-      heat: "",
-      voltage: "",
-      date:"",
-    },
+      device_uid: "",
+      serial_number: "",
+      location: "",
+      lat: 0,
+      lon:0,
+      createdAt:"",
+     updatedAt: "",
+      airReading: [
+        {
+          id: "",
+          aqi: 0,
+          humidity:0,
+          pm01_0: 0,
+          pm02_5: 0,
+          pm10_0: 0,
+          pressure: 0,
+          temperature: 0,
+          voltage: 0,
+          device_uid: "",
+          captured: 0,
+          createdAt: "",
+          updatedAt: "",
+          airQualityReadingId: ""
+        },
+       
+      ],
+      histories: [
+        {
+          id: "",
+          date: "",
+          aqi: 0,
+          pm1_0: 0,
+          pm2_5: 0,
+          pm10_0: 0,
+          aiReadingId: ""
+        },
+      ]
+    }
   ],
 set_filtered_AQI_datas: (state) => set({ AQI_datas: state }),
 
